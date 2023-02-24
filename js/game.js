21f4fc7caf4c09e0c9990eae0e014d8fbb1202aa
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-02-24 10:22:12 -0300

    create card for each character

--- js/game.js
+++ js/game.js
@@ -1,15 +1,30 @@
 const grid = document.querySelector('.grid');
 
+const characters = [
+  'beth',
+  'jerry',
+  'jessica',
+  'morty',
+  'pessoa-passaro',
+  'pickle-rick',
+  'rick',
+  'summer',
+  'meeseeks',
+  'scroopy'
+]
+
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }
 
-const createCard = () => {
+const createCard = (character) => {
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
+
+    front.style.backgroundImage = `url('../images/${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
@@ -17,4 +32,13 @@ const createCard = () => {
     return card;
 }
 
-createCard();
+const loadGame = () => {
+
+    characters.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+    });
+
+}
+
+loadGame();
